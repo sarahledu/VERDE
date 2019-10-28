dbc062e8ff2b0b963981ffb151f9db90a884453f
sarah ledu <sarah.m.ledu@gmail.com>
2019-10-28 18:10:19 +0100

    logique almost done, reste la méthode play à terminer

--- src/rules.js
+++ src/rules.js
@@ -62,24 +62,48 @@ class Partie {
     for (let i = 0; i < this.nbOfFactories; i++) {
       this.factories[i] = this.tilesStack.splice(0, 4);
     }
+    console.log(this.factories);
     return this.factories;
   }
 }
 
 class Player {
   constructor() {
+    this.preparation = [
+      [{ color: "", value: 0 }],
+      [
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 }
+      ],
+      [
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 }
+      ],
+      [
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 },
+        { color: "", value: 0 }
+      ],
+      [{ color: "", value: 0 }, { color: "", value: 0 }]
+    ];
+
     this.grid = [
       [
         { color: "dkb", value: 0, point: 0 },
         { color: "y", value: 0, point: 0 },
-        { color: "r", value: 1, point: 0 },
+        { color: "r", value: 0, point: 0 },
         { color: "br", value: 0, point: 0 },
         { color: "b", value: 0, point: 0 }
       ],
       [
         { color: "b", value: 0, point: 0 },
         { color: "dkb", value: 0, point: 0 },
-        { color: "y", value: 1, point: 0 },
+        { color: "y", value: 0, point: 0 },
         { color: "r", value: 0, point: 0 },
         { color: "br", value: 0, point: 0 }
       ],
@@ -106,6 +130,81 @@ class Player {
       ]
     ];
     this.totalPoints = 0;
+    this.malusPoints = 0;
+  }
+
+  hasEmptySpots(line){
+    var emptySpots =0
+    for (let j=0;j<this.preparation[line].length;j++){
+      if (this.preparation[line][j].value===0){
+        emptySpots += 1
+      }
+    } return emptySpots;
+  }
+  plays(factory, chosenColor, chosenLine) {
+    var colorCounter = 0;
+    var letsKeepPlaying = true;
+    for (let i = 0; i < factory.length; i++) {
+      if (factory[i] === chosenColor) {
+        colorCounter += 1;
+      }
+    }
+    for (let j = 0; j < this.preparation[chosenLine].length; j++) {
+      if (
+        this.preparation[chosenLine][j].color != chosenColor &&
+        this.preparation[chosenLine][j].color != ""
+      ) {
+        console.log(
+          "You cannot place your tile here coquinou, choose another line"
+        );
+        letsKeepPlaying = false;
+      }
+    }
+    for (let j = 0; j < this.grid[chosenLine].length; j++) {
+      if (this.grid[chosenLine][j].color === chosenColor) {
+        if (this.grid[chosenLine][j].value === 1) {
+          console.log(
+            "You cannot place your tile here dummy,choose another line"
+          );
+          letsKeepPlaying = false;
+        }
+      }
+    }
+    this.hasEmptySpots(chosenLine);
+    if (letsKeepPlaying === true) {
+      if (this.preparation[chosenLine].length>=colorCounter){
+        this.malusPoints += colorCounter-this.preparation[chosenLine].length;
+        //colorCounter=
+      } else {
+        
+      }
+      for (let j = 0; j < this.preparation[chosenLine].length; j++) {
+        if (this.preparation[chosenLine][j].value === 0) {
+          this.preparation[chosenLine][j].value = 1;
+          this.preparation[chosenLine][j].color =chosenColor
+        }
+        
+      }
+      console.log(this.preparation);
+     
+    }
+  }
+  makesWall() {
+    for (let i = 0; i < this.preparation.length; i++) {
+      var ligneTot = 0;
+      for (let j = 0; j < this.preparation[i].length; j++) {
+        if (this.preparation[i][j].value === 1) {
+          ligneTot += 1;
+        }
+      }
+      if (ligneTot == this.preparation[i].length) {
+        for (let j = 0; j < this.grid[i].length; j++) {
+          if (this.grid[i][j].color === this.preparation[i][0].color) {
+            this.grid[i][j].value = 1;
+          }
+        }
+      }
+    }
   }
 
   calculatePoints() {
@@ -114,37 +213,47 @@ class Player {
         if (this.grid[i][j].value === 1) {
           if (this.grid[i][j].point === 0) {
             this.grid[i][j].point = 1;
-            if (this.grid[i - 1][j].value === 1) {
+            this.totalPoints += 1;
+            if (i > 0 && this.grid[i - 1][j].value === 1) {
               this.grid[i][j].point += 1;
+              this.totalPoints += 1;
             }
-            if (this.grid[i + 1][j].value === 1) {
+            if (i < this.grid.length - 1 && this.grid[i + 1][j].value === 1) {
               this.grid[i][j].point += 1;
+              this.totalPoints += 1;
             }
-            if (this.grid[i][j - 1].value === 1) {
+            if (j > 0 && this.grid[i][j - 1].value === 1) {
               this.grid[i][j].point += 1;
+              this.totalPoints += 1;
             }
-            if (this.grid[i + 1][j + 1].value === 1) {
+            if (
+              j < this.grid[i].length - 1 &&
+              this.grid[i][j + 1].value === 1
+            ) {
               this.grid[i][j].point += 1;
+              this.totalPoints += 1;
             }
           }
         }
       }
-    } 
-    console.log(this.grid)
-    // const totalPoints = this.grid.reduce((acc, element) => {
-    //     acc = acc + Number(element.point)
-    //     return acc;
-    // }, 0)
+    }
+    console.log(this.grid);
+    console.log(this.totalPoints);
+    return this.totalPoints;
   }
 }
 
 var partie1 = new Partie(2);
-var player1 =new Player();
+var player1 = new Player();
 
 partie1.definesFactoryNb();
 partie1.definesFactories();
 partie1.definesTilesStack();
 partie1.shufflesStack();
 partie1.startTour();
-Player.calculatePoints()
+
 console.log("---------------");
+player1.plays(partie1.factories[1], "r", 1);
+
+player1.makesWall();
+player1.calculatePoints();
